e6e9199a1c6299722932a74b6b8d304c860aadc1
ruan-r-fogaca <ruanr0909@gmail.com>
2025-11-06 22:25:26 -0300

    Frontend IoT

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ===============================
 
 // CONFIGURAÇÃO: substitua pelo ID do seu canal ThingSpeak
-const CHANNEL_ID = "5SLL6W2PQBHABTFU";
+const CHANNEL_ID = "3151361";
 const FIELD = "1";
 const UPDATE_INTERVAL = 5000; // 5 segundos
 
